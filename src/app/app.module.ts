104784ac0be5cac0f340971b3201649e253bea04
imasmi.com <info@imasmi.com>
2022-12-13 00:00:23 +0200

    Structure in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {CoreModule} from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeListComponent,
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
